53020f12f4310194c5aafb91a5a7a33fc31ac93a
Ganesh Arwat <ganesharwat123@gmail.com>
2025-06-26 22:04:48 +0530

    routing issue in vercel

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-  base: '/', // Important for correct asset paths
-  server: {
-    historyApiFallback: true // Optional: good for dev server
-  }
+  plugins: [react()],
+  base: '/',
 });
